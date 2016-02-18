f32a27f961c00efaf169e87a8be564ea794ef0b7
Maarten van den Berg <maartenberg1+git@gmail.com>
2016-02-18 21:59:56 +0100

    Improve datefinder regex, return timedelta
    Datefinder regex has improved and will give less unneeded matches.
    Find_timedelta will now return an actual timedelta instead of a Dinges.
    Dinges has been removed.

--- tombot/datefinder.py
+++ tombot/datefinder.py
@@ -15,36 +15,34 @@ WORDS = [YEAR_WORDS, WEEK_WORDS, DAY_WORDS, HOUR_WORDS, MINUTE_WORDS, SECOND_WOR
 for list in WORDS:
     list.sort(key=len, reverse=True)
 
-YEAR_PART       = '((?P<years>\d+)\s*?({}))?'.format('|'.join(YEAR_WORDS))
-WEEK_PART       = '((?P<weeks>\d+)\s*?({}))?'.format('|'.join(WEEK_WORDS))
-DAY_PART        = '((?P<days>\d+)\s*?({}))?'.format('|'.join(DAY_WORDS))
-HOUR_PART       = '((?P<hours>\d+)\s*?({}))?'.format('|'.join(HOUR_WORDS))
-MINUTE_PART     = '((?P<minutes>\d+)\s*?({}))?'.format('|'.join(MINUTE_WORDS))
-SECOND_PART     = '((?P<seconds>\d+)\s*?({}))?'.format('|'.join(SECOND_WORDS))
-
-FUTURE_MARKERS  = ['in', 'over', 'na']
 SEPARATORS      = [r'\s', ',', 'en', 'and', '&']
 SEP_PART        = '({})*'.format('|'.join(SEPARATORS))
 
-MONSTER         = SEP_PART.join([YEAR_PART, WEEK_PART, DAY_PART, HOUR_PART, MINUTE_PART, SECOND_PART])
-REGEX           = re.compile(MONSTER, re.IGNORECASE)
-print(MONSTER)
+YEAR_PART       = '((?P<years>\d+)\s*?({}){})?'.format('|'.join(YEAR_WORDS), SEP_PART)
+WEEK_PART       = '((?P<weeks>\d+)\s*?({}){})?'.format('|'.join(WEEK_WORDS), SEP_PART)
+DAY_PART        = '((?P<days>\d+)\s*?({}){})?'.format('|'.join(DAY_WORDS), SEP_PART)
+HOUR_PART       = '((?P<hours>\d+)\s*?({}){})?'.format('|'.join(HOUR_WORDS), SEP_PART)
+MINUTE_PART     = '((?P<minutes>\d+)\s*?({}){})?'.format('|'.join(MINUTE_WORDS), SEP_PART)
+SECOND_PART     = '((?P<seconds>\d+)\s*?({}))?'.format('|'.join(SECOND_WORDS))
 
-Dinges = namedtuple('Dinges', 'years days hours minutes seconds')
+FUTURE_MARKERS  = ['in', 'over', 'na']
+
+MONSTER         = ''.join([YEAR_PART, WEEK_PART, DAY_PART, HOUR_PART, MINUTE_PART, SECOND_PART])
+REGEX           = re.compile(MONSTER, re.IGNORECASE)
+
 def find_timedelta(text):
     matches = REGEX.findall(text)
     for res in matches:
         if any(res):
             match = res
             break
-    years   = int(match[1]) if match[1] else 0
-    weeks   = int(match[5]) if match[5] else 0
-    days    = int(match[9]) if match[9] else 0
-    tdays   = days + 7 * weeks
-    hours   = int(match[13]) if match[13] else 0
-    minutes = int(match[17]) if match[17] else 0
-    seconds = int(match[21]) if match[21] else 0
-    result  = Dinges(
-        years=years, days=tdays, hours=hours,
-        minutes=minutes, seconds=seconds)
+    years   = int(match[1]) if match[1] else 0 # in tdays
+    weeks   = int(match[5]) if match[5] else 0 # in tdays
+    days    = int(match[9]) if match[9] else 0 # in tdays
+    tdays   = days + 7 * weeks + 365 * years
+    hours   = int(match[13]) if match[13] else 0 # passed
+    minutes = int(match[17]) if match[17] else 0 # passed
+    seconds = int(match[21]) if match[21] else 0 # passed
+    result  = timedelta(
+        days=tdays, hours=hours, minutes=minutes, seconds=seconds)
     return result
