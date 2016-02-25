94ecb678f1b44a7192e7c4fa4588f932d28537a9
Maarten van den Berg <schoenveter123+git@gmail.com>
2016-02-25 19:02:59 +0100

    More expressive error

--- tombot/plugins/reminder_plugin.py
+++ tombot/plugins/reminder_plugin.py
@@ -41,6 +41,9 @@ def addreminder_cb(bot, message, *args, **kwargs):
         deadline = delta
     else:
         deadline = trytime
+    if deadline < datetime.datetime.now():
+        logger.error('Parsing %s failed, invalid deadline')
+        return 'Sorry, limitations prevent parsing that kind of time.'
     LOGGER.debug('Parsed reminder command "%s"', body)
     LOGGER.info('Deadline %s from message "%s".',
                 deadline, body)
